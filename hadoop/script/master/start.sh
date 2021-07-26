c010583dcc68ef4fe43e9c2c783e4bd4727f2fa9
John Sundarraj <johnsundarraj88@gmail.com>
2021-07-26 18:59:02 +0530

    Hadoop node hostname/IP checks disabled; Included basic Kubernetes templates;

--- hadoop/script/master/start.sh
+++ hadoop/script/master/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -d '/opt/hadoop/data/hdfs' ]; then
-  /usr/local/lib/hadoop/bin/hdfs namenode -format
+  /usr/local/lib/hadoop/bin/hdfs namenode -format hadoop-cluster
 fi
 /usr/local/lib/hadoop/sbin/hadoop-daemon.sh start namenode
